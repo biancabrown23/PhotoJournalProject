b7d40569ad4873748b7b7bf5382ebb2a7bc171c6
Bee <biancabrown@pursuit.org>
2019-10-09 14:24:12 -0400

    Added View Controllers to Xcode.

---
+++ PhotoJournal/View Controllers/UserEntriesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UserEntriesViewController.swift
+//  PhotoJournal
+//
+//  Created by Bianca Brown on 10/9/19.
+//  Copyright © 2019 Bee. All rights reserved.
+//
+
+import UIKit
+
+class UserEntriesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
