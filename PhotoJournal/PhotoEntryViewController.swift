3b049078b168d86a19d7fe221e0b230794c572c5
Bee <biancabrown@pursuit.org>
2019-10-08 21:28:58 -0400

    Completed basic view of the storyboard.

---
+++ PhotoJournal/PhotoEntryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoJournal
+//
+//  Created by Bianca Brown on 10/8/19.
+//  Copyright © 2019 Bee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
